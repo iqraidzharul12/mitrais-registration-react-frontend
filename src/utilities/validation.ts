cc450d7f5d3977159f1bb360cc056406ac5d20df
iqraidzharul12 <iqra.idzharul.haq@gmail.com>
2020-09-14 15:01:23 +0700

    regex email fix

--- src/utilities/validation.ts
+++ src/utilities/validation.ts
@@ -4,7 +4,9 @@ export const validateMobile = (string: string) => {
   );
 };
 export const validateEmail = (string: string) => {
-  return string.match(/^([a-zA-Z0-9_]+)@([a-zA-Z0-9_]+)\.([a-zA-Z]{2,5})$/);
+  return string.match(
+    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+  );
 };
 export const validateRequired = (string: string) => {
   return string !== "";
